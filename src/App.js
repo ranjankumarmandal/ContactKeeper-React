de924abbd6fa1dcf7d3d065dcac3c5de2192f323
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-11-08 06:20:28 +0530

    ContactKeeper application - navbar component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './Components/layout/Navbar';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Navbar />
+      My App
+    </div>
+  );
+};
+
+export default App;
