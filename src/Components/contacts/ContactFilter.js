cb4161131c8c33d8e1784eb26c4c53f6bdb90726
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2021-11-15 02:46:44 +0530

    contact filter, form & state

--- src/Components/contacts/ContactFilter.js
+++ src/Components/contacts/ContactFilter.js
@@ -1,10 +1,34 @@
-import React, { useContext, useRef } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import contactContext from '../../context/contact/contactContext';
 
 const ContactFilter = () => {
   const contactContext1 = useContext(contactContext);
+  const text = useRef('');
 
-  return <div></div>;
+  const { filterContacts, clearFilter, filtered } = contactContext1;
+
+  useEffect(() => {
+    if (filtered === null) text.current.value = '';
+  });
+
+  const onchange = (e) => {
+    if (text.current.value !== '') {
+      filterContacts(e.target.value);
+    } else {
+      clearFilter();
+    }
+  };
+
+  return (
+    <form>
+      <input
+        ref={text}
+        type='text'
+        placeholder='Filter Contacts...'
+        onChange={onchange}
+      />
+    </form>
+  );
 };
 
 export default ContactFilter;
--- src/Components/contacts/Contacts.js
+++ src/Components/contacts/Contacts.js
@@ -1,17 +1,30 @@
 import React, { Fragment, useContext } from 'react';
 import contactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const Contacts = () => {
   const contactContext1 = useContext(contactContext);
 
-  const { contacts } = contactContext1;
+  const { contacts, filtered } = contactContext1;
+
+  if (contacts.length === 0) return <h4>Please add a contact</h4>;
 
   return (
     <Fragment>
-      {contacts.map((contact) => (
-        <ContactItem key={contact.id} contact={contact} />
-      ))}
+      <TransitionGroup>
+        {filtered !== null
+          ? filtered.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))
+          : contacts.map((contact) => (
+              <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                <ContactItem contact={contact} />
+              </CSSTransition>
+            ))}
+      </TransitionGroup>
     </Fragment>
   );
 };
